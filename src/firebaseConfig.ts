72838cd5b01ed975f819ec79357fabd30b5041b7
Shostyy <anatoliy.shostal@gmail.com>
2026-03-17 10:20:12 +0200

    changes

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDTXQTVuGPg-KXcc1EXXWr7Bmyin-CRaD8",
-  authDomain: "adoniswastetracker.firebaseapp.com",
+  apiKey: "AIzaSyDWqObVH51U2KQntIRzIrMRhYtQtVxHH_Q",
+  authDomain: "minska-fortune-wheel.firebaseapp.com",
   databaseURL:
-    "https://adoniswastetracker-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "adoniswastetracker",
-  storageBucket: "adoniswastetracker.appspot.com",
-  messagingSenderId: "468226752735",
-  appId: "1:468226752735:web:2a35022fc4f970094ea96a",
-  measurementId: "G-MQR3ZKT2MT",
+    "https://minska-fortune-wheel-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "minska-fortune-wheel",
+  storageBucket: "minska-fortune-wheel.firebasestorage.app",
+  messagingSenderId: "1021396006262",
+  appId: "1:1021396006262:web:8cd3d0cd152e558df6f657",
+  measurementId: "G-2ZGWL9YX78",
 };
